de863fb64b4105dc1d367c63ce63d6d3ec82a2c1
YuHeng Sun <butsalt@163.com>
2017-01-02 19:29:56 +0800

    add comment

--- src/core/access.js
+++ src/core/access.js
@@ -6,6 +6,8 @@ define( [
 
 // Multifunctional method to get and set values of a collection
 // The value/s can optionally be executed if it's a function
+// 如果key上要设置value，raw必须设为true
+// 否则value会被作为原始value的filter被调用
 var access = function( elems, fn, key, value, chainable, emptyGet, raw ) {
 	var i = 0,
 		len = elems.length,
@@ -13,8 +15,10 @@ var access = function( elems, fn, key, value, chainable, emptyGet, raw ) {
 
 	// Sets many values
 	if ( jQuery.type( key ) === "object" ) {
+		// 设置完毕后返回集合本身
 		chainable = true;
 		for ( i in key ) {
+			// key是键值对对象，i是当前遍历到的key，key[i]是key对应的value
 			access( elems, fn, i, key[ i ], true, emptyGet, raw );
 		}
 
@@ -22,11 +26,13 @@ var access = function( elems, fn, key, value, chainable, emptyGet, raw ) {
 	} else if ( value !== undefined ) {
 		chainable = true;
 
+		// value如果不是function，则只能作为值被设置
 		if ( !jQuery.isFunction( value ) ) {
 			raw = true;
 		}
 
 		if ( bulk ) {
+			// key为空
 
 			// Bulk operations run against the entire set
 			if ( raw ) {
@@ -47,12 +53,15 @@ var access = function( elems, fn, key, value, chainable, emptyGet, raw ) {
 				fn(
 					elems[ i ], key, raw ?
 					value :
+					// 通过执行fn获取值
+					// 通过执行value获取实际要设置的值
 					value.call( elems[ i ], i, fn( elems[ i ], key ) )
 				);
 			}
 		}
 	}
 
+	// set完毕，返回集合本身
 	if ( chainable ) {
 		return elems;
 	}
@@ -62,6 +71,7 @@ var access = function( elems, fn, key, value, chainable, emptyGet, raw ) {
 		return fn.call( elems );
 	}
 
+	// 如果第一个元素则返回第一个元素属性为key的值否则返回emptyGet(默认值)
 	return len ? fn( elems[ 0 ], key ) : emptyGet;
 };
 
